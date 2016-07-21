8047bb8a9f6cdb2d72571c1ee7915d604607b0f9
Peter Hebert <peter@peterhebert.com>
2016-07-21 16:49:51 -0700

    removed unnneeded twig templates for post formats converted search template to Twig

--- search.php
+++ search.php
@@ -1,43 +1,16 @@
 <?php
 /**
  * The template for displaying search results pages.
- *
- * @package Simple Grey
- */
+  *
+  * Methods for TimberHelper can be found in the /lib sub-directory
+  *
+  * @package Simple Grey
+  * @subpackage  Timber
+  * @since    Timber 0.1
+  */
 
-get_header(); ?>
-
-		<main id="main" class="site-main" role="main">
-
-		<?php if ( have_posts() ) : ?>
-
-			<header class="page-header">
-				<h1 class="page-title"><?php printf( __( 'Search Results for: %s', 'simple-grey' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
-			</header><!-- .page-header -->
-
-			<?php /* Start the Loop */ ?>
-			<?php while ( have_posts() ) : the_post(); ?>
-
-				<?php
-				/**
-				 * Run the loop for the search to output the results.
-				 * If you want to overload this in a child theme then include a file
-				 * called content-search.php and that will be used instead.
-				 */
-				get_template_part( 'content', 'search' );
-				?>
-
-			<?php endwhile; ?>
-
-			<?php simple_grey_paging_nav(); ?>
-
-		<?php else : ?>
-
-			<?php get_template_part( 'content', 'none' ); ?>
-
-		<?php endif; ?>
-
-		</main><!-- #main -->
-
-<?php get_sidebar(); ?>
-<?php get_footer(); ?>
+$context = Timber::get_context();
+$post = Timber::query_post();
+$context['post'] = $post;
+$context['search_query'] = get_search_query();
+Timber::render( array( 'search.twig' ), $context );
