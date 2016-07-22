9ed4b552155d1deed71b9bbee031cd83f61bdb43
Peter Hebert <peter@peterhebert.com>
2016-07-22 16:36:48 -0700

    converted 404 error page template to Twig added template framework for error pages changed 'content' Twig block to 'main' for primary templates added more side margin on image-left and image-right floats removed button styling from post navigation, only for posts pages made teaser the base content template renamed content-single to content/single-post renamed content-page to content/single-page renamed content-none to content/none

--- 404.php
+++ 404.php
@@ -5,50 +5,11 @@
  * @package Simple Grey
  */
 
-get_header(); ?>
+$context = Timber::get_context();
+$post = new TimberPost();
+$context['post'] = $post;
+$context['error_code'] = 404;
+$context['error_class'] = 'not-found';
+$context['title'] = __( '404 Error - Not Found', 'simple-grey' );
 
-    <main id="main" role="main">
-
-			<section class="error-404 not-found">
-				<header class="page-header">
-					<h1 class="page-title"><?php _e( 'Oops! That page can&rsquo;t be found.', 'simple-grey' ); ?></h1>
-				</header><!-- .page-header -->
-
-				<div class="page-content">
-					<p><?php _e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'simple-grey' ); ?></p>
-
-					<?php get_search_form(); ?>
-
-					<?php the_widget( 'WP_Widget_Recent_Posts' ); ?>
-
-					<?php if ( simple_grey_categorized_blog() ) : // Only show the widget if site has multiple categories. ?>
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php _e( 'Most Used Categories', 'simple-grey' ); ?></h2>
-						<ul>
-						<?php
-							wp_list_categories( array(
-								'orderby'    => 'count',
-								'order'      => 'DESC',
-								'show_count' => 1,
-								'title_li'   => '',
-								'number'     => 10,
-							) );
-						?>
-						</ul>
-					</div><!-- .widget -->
-					<?php endif; ?>
-
-					<?php
-						/* translators: %1$s: smiley */
-						$archive_content = '<p>' . sprintf( __( 'Try looking in the monthly archives. %1$s', 'simple-grey' ), convert_smilies( ':)' ) ) . '</p>';
-						the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
-					?>
-
-					<?php the_widget( 'WP_Widget_Tag_Cloud' ); ?>
-
-				</div><!-- .page-content -->
-			</section><!-- .error-404 -->
-
-		</main><!-- #main -->
-
-<?php get_footer(); ?>
+Timber::render( array( 'error.twig' ), $context );
--- archive.php
+++ archive.php
@@ -16,58 +16,66 @@
 
 $templates = array( 'archive.twig', 'index.twig' );
 
-$data = Timber::get_context();
+$context = Timber::get_context();
 
-$data['title'] = __( 'Archives', 'simple-grey' );
+$context['title'] = __( 'Archives', 'simple-grey' );
 
 if ( is_day() ) {
-  $data['title'] = sprintf( __( 'Day: %s', 'simple-grey' ), get_the_date( _x( 'F j, Y', 'daily archives date format', 'simple-grey' ) ) );
+  $context['title'] = sprintf( __( 'Day: %s', 'simple-grey' ), get_the_date( _x( 'F j, Y', 'daily archives date format', 'simple-grey' ) ) );
 } else if ( is_month() ) {
-  $data['title'] = sprintf( __( 'Month: %s', 'simple-grey' ), get_the_date( _x( 'F Y', 'monthly archives date format', 'simple-grey' ) ) );
+  $context['title'] = sprintf( __( 'Month: %s', 'simple-grey' ), get_the_date( _x( 'F Y', 'monthly archives date format', 'simple-grey' ) ) );
 } else if ( is_year() ) {
-  $data['title'] = sprintf( __( 'Year: %s', 'simple-grey' ), get_the_date( _x( 'Y', 'yearly archives date format', 'simple-grey' ) ) );
+  $context['title'] = sprintf( __( 'Year: %s', 'simple-grey' ), get_the_date( _x( 'Y', 'yearly archives date format', 'simple-grey' ) ) );
 } else if ( is_tag() ) {
-  $data['title'] = sprintf( __( 'Tag: %s', 'simple-grey' ), single_tag_title( '', false ) );
+  $context['title'] = sprintf( __( 'Tag: %s', 'simple-grey' ), single_tag_title( '', false ) );
 } else if ( is_category() ) {
-  $data['title'] = sprintf( __( 'Category: %s', 'simple-grey' ), single_cat_title( '', false ) );
+  $context['title'] = sprintf( __( 'Category: %s', 'simple-grey' ), single_cat_title( '', false ) );
 	array_unshift( $templates, 'archive-' . get_query_var( 'cat' ) . '.twig' );
 } else if ( is_author() ) {
-  $data['title'] = sprintf( __( 'Author: %s', 'simple-grey' ), '<span class="vcard">' . get_the_author() . '</span>' );
+  $context['title'] = sprintf( __( 'Author: %s', 'simple-grey' ), '<span class="vcard">' . get_the_author() . '</span>' );
 } else if ( is_tax( 'post_format', 'post-format-aside' ) ) {
-  $data['title'] = _x( 'Asides', 'post format archive title', 'simple-grey' );
+  $context['title'] = _x( 'Asides', 'post format archive title', 'simple-grey' );
 } else if ( is_tax( 'post_format', 'post-format-gallery' ) ) {
-  $data['title'] = _x( 'Galleries', 'post format archive title', 'simple-grey' );
+  $context['title'] = _x( 'Galleries', 'post format archive title', 'simple-grey' );
 } else if ( is_tax( 'post_format', 'post-format-image' ) ) {
-  $data['title'] = _x( 'Images', 'post format archive title', 'simple-grey' );
+  $context['title'] = _x( 'Images', 'post format archive title', 'simple-grey' );
 } else if ( is_tax( 'post_format', 'post-format-video' ) ) {
-  $data['title'] = _x( 'Videos', 'post format archive title', 'simple-grey' );
+  $context['title'] = _x( 'Videos', 'post format archive title', 'simple-grey' );
 } else if ( is_tax( 'post_format', 'post-format-quote' ) ) {
-  $data['title'] = _x( 'Quotes', 'post format archive title', 'simple-grey' );
+  $context['title'] = _x( 'Quotes', 'post format archive title', 'simple-grey' );
 } else if ( is_tax( 'post_format', 'post-format-link' ) ) {
-  $data['title'] = _x( 'Links', 'post format archive title', 'simple-grey' );
+  $context['title'] = _x( 'Links', 'post format archive title', 'simple-grey' );
 } else if ( is_tax( 'post_format', 'post-format-status' ) ) {
-  $data['title'] = _x( 'Statuses', 'post format archive title', 'simple-grey' );
+  $context['title'] = _x( 'Statuses', 'post format archive title', 'simple-grey' );
 } else if ( is_tax( 'post_format', 'post-format-audio' ) ) {
-  $data['title'] = _x( 'Audio', 'post format archive title', 'simple-grey' );
+  $context['title'] = _x( 'Audio', 'post format archive title', 'simple-grey' );
 } else if ( is_tax( 'post_format', 'post-format-chat' ) ) {
-  $data['title'] = _x( 'Chats', 'post format archive title', 'simple-grey' );
+  $context['title'] = _x( 'Chats', 'post format archive title', 'simple-grey' );
 } else if ( is_tax() ) {
   $tax = get_taxonomy( get_queried_object()->taxonomy );
   /* translators: 1: Taxonomy singular name, 2: Current taxonomy term */
-  $data['title'] = sprintf( __( '%1$s: %2$s', 'simple-grey' ), $tax->labels->singular_name, single_term_title( '', false ) );
+  $context['title'] = sprintf( __( '%1$s: %2$s', 'simple-grey' ), $tax->labels->singular_name, single_term_title( '', false ) );
   /* set template suggestion */
   // array_unshift( $templates, 'taxonomy-' . $tax->name . '.twig' );
 } else if ( is_post_type_archive() ) {
-  $data['title'] = sprintf( __( 'Archives: %s', 'simple-grey' ), post_type_archive_title( '', false ) );
+  $context['title'] = sprintf( __( 'Archives: %s', 'simple-grey' ), post_type_archive_title( '', false ) );
 	array_unshift( $templates, 'archive-' . get_post_type() . '.twig' );
 }
 
 // archive description
 if ( is_tag() ||  is_category() || is_tax() ) {
-  $data['archive_description'] = apply_filters( 'get_the_archive_description', term_description() );
+  $context['archive_description'] = apply_filters( 'get_the_archive_description', term_description() );
 }
 
-$data['posts'] = Timber::get_posts();
+$context['posts'] = Timber::get_posts();
 $context['pagination'] = Timber::get_pagination();
 
-Timber::render( $templates, $data );
+$context['post_thumb_classes'] = 'image-left';
+$context['post_classes'] = 'post-teaser';
+
+$page_for_posts = get_option( 'page_for_posts' );
+if( 0 < $page_for_posts) {
+  $context['posts_page'] = new Timber\Post($page_for_posts);
+}
+
+Timber::render( $templates, $context );
