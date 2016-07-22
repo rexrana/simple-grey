b6c569f74b7828e6b2d2235b692662da8ce5affe
Peter Hebert <peter@peterhebert.com>
2016-07-22 16:53:14 -0700

    converted Maintenance template to Twig

--- maintenance.php
+++ maintenance.php
@@ -11,23 +11,6 @@ $protocol = 'HTTP/1.0';
 header( "$protocol 503 Service Unavailable", true, 503 );
 header( 'Content-Type: text/html; charset=utf-8' );
 
-get_header(); ?>
+$context = Timber::get_context();
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
-
-			<section class="maintenance-mode">
-				<header class="page-header">
-					<h1 class="page-title"><?php _e( 'Site Under Maintenance', 'simple-grey' ); ?></h1>
-				</header><!-- .page-header -->
-
-				<div class="page-content">
-					<p><?php _e( "We're currently performing maintenance on this website. Please check back soon.", 'simple-grey' ); ?></p>
-
-				</div><!-- .page-content -->
-			</section><!-- .maintenance-mode -->
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
-
-<?php get_footer(); ?>
+Timber::render( array( 'maintenance.twig' ), $context );
